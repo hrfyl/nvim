8679f7406550c6f96ff0f87f2c98796ffc868fe3
hrfyl <fengleyll@gmail.com>
2023-10-05 15:20:27 +0800

    update init.lua

--- init.lua
+++ init.lua
@@ -10,5 +10,7 @@ require('autocmds')         -- auto command
 require('colorscheme')      -- 主题配置
 
 -- lsp和插件配置
+require('dap/setup')        -- 加载dap配置
 require('lsp/setup')        -- 加载lsp配置
+require('utils/setup')      -- 加载utils配置
 require('plugins/setup')    -- 加载三方插件配置
